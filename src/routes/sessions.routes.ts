092c31a8276e1fc20e22dc971c25a37f654c835d
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-07-07 03:42:02 -0400

    Add token middleware to ensure the user is authenticated

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -10,14 +10,14 @@ sessionsRouter.post("/", async (req, res) => {
 
 		const userService = new AuthenticateUserService();
 
-		const { user } = await userService.execute({
+		const { user, token } = await userService.execute({
 			email,
 			password,
 		});
 
 		delete user.password;
 
-		return res.json({ user });
+		return res.json({ user, token });
 	} catch (err) {
 		return res.status(400).json({ error: err.message });
 	}
