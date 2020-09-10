70cb03a38dc7e01ad141462b79622824e55d157e
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-09-10 04:54:06 -0400

    Add Service to check provider availability by month

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,7 +1,8 @@
-import { getRepository, Repository } from "typeorm";
+import { getRepository, Repository, Raw } from "typeorm";
 
-import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
-import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO'
+import IAppointmentsRepository from "@modules/appointments/repositories/IAppointmentsRepository";
+import ICreateAppointmentDTO from "@modules/appointments/dtos/ICreateAppointmentDTO";
+import IFindAllInMonthFromProviderDTO from "@modules/appointments/dtos/IFindAllInMonthFromProviderDTO";
 
 import Appointment from "../entities/Appointment";
 
@@ -20,10 +21,33 @@ class AppointmentsRepository implements IAppointmentsRepository {
 		return findAppointment;
 	}
 
-	public async create({ provider_id, date }: ICreateAppointmentDTO): Promise<Appointment> {
+	public async findByAllInMonthFromProvider({
+		provider_id,
+		month,
+		year,
+	}: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+		const parsedMonth = String(month).padStart(2, "0");
+
+		const appointments = await this.ormRepository.find({
+			where: {
+				provider_id,
+				date: Raw(
+					dateFieldName =>
+						`to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`
+				),
+			},
+		});
+
+		return appointments;
+	}
+
+	public async create({
+		provider_id,
+		date,
+	}: ICreateAppointmentDTO): Promise<Appointment> {
 		const appointment = this.ormRepository.create({
 			provider_id,
-			date
+			date,
 		});
 
 		await this.ormRepository.save(appointment);
