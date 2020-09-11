fca6b57f8378ff641309491a1dbf2fcc7c06f977
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-09-11 18:42:31 -0400

    Add user_id field to appointments table

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -6,6 +6,7 @@ import CreateAppointmentService from "@modules/appointments/services/CreateAppoi
 
 export default class AppointmentsController {
 	public async create(req: Request, res: Response): Promise<Response> {
+		const user_id = req.user.id;
 		const { provider_id, date } = req.body;
 
 		const parsedDate = parseISO(date);
@@ -17,6 +18,7 @@ export default class AppointmentsController {
 		const appointment = await createAppointmentService.execute({
 			date: parsedDate,
 			provider_id,
+			user_id,
 		});
 
 		return res.json(appointment);
