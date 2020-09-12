a726216e453a951d3af0286b73c1222bb2ff7478
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-09-12 16:32:14 -0400

    Add route to list appointments by day or month

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour, isBefore } from "date-fns";
+import { startOfHour, isBefore, getHours } from "date-fns";
 import { injectable, inject } from "tsyringe";
 
 import AppError from "@shared/errors/AppError";
@@ -30,12 +30,22 @@ class CreateAppointmentService {
 			throw new AppError("You can't create an appointment on a past date");
 		}
 
+		if (user_id === provider_id) {
+			throw new AppError("You can't create an appointment with yourself");
+		}
+
 		const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
 			appointmentDate
 		);
 
 		if (findAppointmentInSameDate) {
 			throw new AppError("This appointment is already booked");
+		}
+
+		if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+			throw new AppError(
+				"You can only create appointments between 8am and 5pm"
+			);
 		}
 
 		const appointment = await this.appointmentsRepository.create({
