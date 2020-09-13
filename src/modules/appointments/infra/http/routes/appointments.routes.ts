6ec0bb820bb6ac07f7990565fc7091475600d586
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-09-13 00:34:05 -0400

    Add celebrate to validate requests

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { celebrate, Segments, Joi } from "celebrate";
 
 import ensureAuthenticated from "@modules/users/infra/http/middlewares/ensureAuthenticated";
 import AppointmentsController from "../controllers/AppointmentsController";
@@ -10,7 +11,16 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.post("/", appointmnetsController.create);
 appointmentsRouter.get("/me", providerAppointmentsController.index);
+appointmentsRouter.post(
+	"/",
+	celebrate({
+		[Segments.BODY]: {
+			provider_id: Joi.string().uuid().required(),
+			date: Joi.date(),
+		},
+	}),
+	appointmnetsController.create
+);
 
 export default appointmentsRouter;
