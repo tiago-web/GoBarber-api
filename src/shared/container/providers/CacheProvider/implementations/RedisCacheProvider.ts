e673e9ab5558250fec6aaa13017e6bf665fe3335
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-09-15 07:28:21 -0400

    ADD ormconfig.json file again after acidental deleting

---
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -0,0 +1,18 @@
+import Redis, { Redis as RedisClient } from "ioredis";
+import cacheConfig from "@config/cache";
+
+import ICacheProvider from "../models/ICacheProvider";
+
+export default class RedisCacheProvider implements ICacheProvider {
+	private client: RedisClient;
+
+	constructor() {
+		this.client = new Redis(cacheConfig.config.redis);
+	}
+
+	save(key: string, value: string): Promise<void> {}
+
+	recover(key: string): Promise<string> {}
+
+	invalidate(key: String): Promise<void> {}
+}
