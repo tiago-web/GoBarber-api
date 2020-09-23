b2e1153ea3f763072fa691f7f007b47f3764b067
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-09-23 15:53:45 -0400

    Change request params to query params on all get requests

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -4,6 +4,7 @@ import ICacheProvider from "@shared/container/providers/CacheProvider/models/ICa
 
 import IAppointmentsRepository from "../repositories/IAppointmentsRepository";
 import Appointment from "../infra/typeorm/entities/Appointment";
+import { classToClass } from "class-transformer";
 
 interface IRequest {
 	provider_id: string;
@@ -44,7 +45,7 @@ class ListProviderAppointmentsService {
 				}
 			);
 
-			await this.cacheProvider.save(cacheKey, appointments);
+			await this.cacheProvider.save(cacheKey, classToClass(appointments));
 		}
 
 		return appointments;
